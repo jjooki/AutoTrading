38f0da97c42b6daa78110ed1c01dc38a06ab0706
jjooki <piy8117982@gmail.com>
2022-07-23 00:51:23 +0900

    2. pyupbit practice

--- order.py
+++ order.py
@@ -9,9 +9,24 @@ access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
 secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
 server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
 
+params = {
+  'market': 'KRW-BTC'
+  'side': 'bid'
+  'ord_type': 'limit'
+  'price': '100.0'
+  'volume': '0.01'
+}
+query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+m = hashlib.sha512()
+m.update(query_string)
+query_hash = m.hexdigest()
+
 payload = {
     'access_key': access_key,
     'nonce': str(uuid.uuid4()),
+    'query_hash': query_hash,
+    'query_hash_alg': 'SHA512',
 }
 
 jwt_token = jwt.encode(payload, secret_key)
@@ -20,5 +35,5 @@ headers = {
   'Authorization': authorization,
 }
 
-res = requests.get(server_url + '/v1/accounts', headers=headers)
+res = requests.post(server_url + '/v1/orders', json=data, headers=headers)
 res.json()
