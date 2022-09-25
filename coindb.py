04b0e01becec4da0c40a3503823fc199484c8cb8
jjooki <piy8117982@gmail.com>
2022-09-26 04:51:21 +0900

    feat: Edit coinDB.execute()

--- coindb.py
+++ coindb.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 class coinDB():
-    def __init__(self, host, exchange='upbit'):
+    def __init__(self, host='52.79.239.69', exchange='upbit'):
         self.db = pymysql.connect(host=host,
                                   port=3306,
                                   user='root',
@@ -16,8 +16,14 @@ class coinDB():
         self.cursor = self.db.cursor()
         
     def execute(self, query):
-        self.cursor.execute(query)
-        return pd.DataFrame(self.cursor.fetchall())
+        try:
+            self.cursor.execute(query)
+            if ('SELECT' in query) or ('SHOW' in query):
+                return pd.DataFrame(self.cursor.fetchall())
+            else:
+                return pd.DataFrame()
+        except pymysql.err.ProgrammingError as e:
+            print(e)
 
     def rollback(self):
         self.db.rollback()
@@ -28,9 +34,11 @@ class coinDB():
 if __name__ == '__main__':
     host = '52.79.239.69'
     upbitDB = coinDB(host)
-    query = """SHOW DATABASES;"""
+    query = "SHOW DATABASES;"
+    result = upbitDB.execute(query)
+    print(result)
+    upbitDB.execute("USE upbit;")
+    query = "SELECT DATABASE();"
     result = upbitDB.execute(query)
     print(result)
     upbitDB.close()
-
-
