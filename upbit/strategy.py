f3c1062d6e57fa3b5b7a9519e1cc233c7248c9de
jjooki <piy8117982@gmail.com>
2022-10-11 23:48:02 +0900

    feat: Create MA, BollingerBand function.

--- upbit/strategy.py
+++ upbit/strategy.py
@@ -1,8 +1,12 @@
 """
 1.단기
- 1) Short Golden Cross(SGC) : 10분봉 골든크로스 매수/데드크로스 매도전략
+ 1) Short Golden Cross(SGC) : 5분봉 골든크로스 매수/데드크로스 매도전략
  2) 1시간 변동성 돌파 전략
  3) 1% 초단타 전략
+
+ parameter :
+ 1. period
+ 2. 
 """
 
 """
@@ -11,3 +15,19 @@
  2) 변동성 돌파 전략
  3) 10% 중단기 전략
 """
+import pandas as pd
+
+class Strategy:
+    def __init__(self):
+        pass
+    
+    def MA(self, data: pd.DataFrame, interval: int=5) -> pd.Series:
+        return data.rolling(interval).mean()
+
+    def Bollinger(self, data: pd.DataFrame) -> pd.DataFrame:
+        bollinger_upper = data.rolling(20).mean() + data.rolling(20).std() * 2
+        bollinger_lower = data.rolling(20).mean() - data.rolling(20).std() * 2
+        ma20 = self.MA(data, interval=20)
+        
+    def SGC(self):
+        pass
