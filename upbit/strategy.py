89e748ef059fe058d2baba3f466882b2dc70a8f6
jjooki <piy8117982@gmail.com>
2022-10-12 04:45:50 +0900

    feat: Edit Short_Strategy

--- upbit/strategy.py
+++ upbit/strategy.py
@@ -21,35 +21,43 @@ parameter :
 3. 장기 전략
  1) LGC : 
 """
+import numpy as np
 import pandas as pd
 import pyupbit
 
 class Strategy:
     def __init__(self, ticker: str, interval: str, count: int) -> None:
         self.interval = int(interval[6:])
-        self.data = pyupbit.get_ohlcv(ticker=ticker, interval=interval, count=count)
+        self.ohlcv = pyupbit.get_ohlcv(ticker=ticker, interval=interval, count=count)
+        # Linear Interpolation으로 결측치 채우기
+        self.ohlcv = self.ohlcv.interpolate(method='linear', limit_direction='forward')
+        self.data = self.ohlcv.close
     
     def MA(self, interval: int=5) -> pd.Series:
         return self.data.rolling(interval).mean()
+
+    def WMA(self, interval: int=5) -> pd.Series:
+        weights = np.arange(1, 1 + interval)
+        return self.data.rolling(interval).apply(lambda prices: np.dot(prices, weights)/weights.sum(), raw=True)
 
     def Bollinger(self) -> pd.DataFrame:
         bollinger_upper = self.MA(self.data, interval=20) + self.data.rolling(20).std() * 2
         bollinger_lower = self.MA(self.data, interval=20) - self.data.rolling(20).std() * 2
         ma20 = self.MA(self.data, interval=20)
         return pd.concat([bollinger_lower, ma20, bollinger_upper],
-                          columns=['lower', 'mean', 'upper'])
+                         columns=['lower', 'mean', 'upper'])
 
 
 
 class Short_Strategy(Strategy):
-    def __init__(self, ticker: str="KRW-BTC", interval: str="minute15", count: int=200) -> None:
+    def __init__(self, ticker="KRW-BTC", interval="minute15", count=200) -> None:
         super().__init__(ticker, interval, count)
     
     def SGC(self):
         """
         Short Golden Cross: 매수시점 기준
         """
-        check1 = self.MA(60)[-1-] - self.MA(5)[-5] > 0
+        check1 = self.MA(60).iloc[-1 - (60 / self.interval)] - self.MA(5).iloc[-1 - (60 / self.interval)] > 0
         check2 = self.MA(60)[-1] - self.MA(5)[-1] < 0
         return check1 & check2
 
@@ -57,8 +65,6 @@ class Short_Strategy(Strategy):
         """
         Short Dead Cross: 매도시점 기준
         """
-        if 'min' in self.interval:
-            time = 60 / int(self.interval[6:])
-        check1 = self.MA(60)[-5] - self.MA(5)[-5] < 0
+        check1 = self.MA(60).iloc[-1 - (60 / self.interval)] - self.MA(5).iloc[-1 - (60 / self.interval)] < 0
         check2 = self.MA(60)[-1] - self.MA(5)[-1] > 0
         return check1 & check2
